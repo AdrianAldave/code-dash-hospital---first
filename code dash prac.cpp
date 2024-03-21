58c7ef5a699017c15036766b5ea11a1ebc37f229
Ahri <154325611+AdrianAldave@users.noreply.github.com>
2024-03-21 12:33:07 +0800

    Update code dash prac.cpp

--- code dash prac.cpp
+++ code dash prac.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 
 struct Hospital {
+	
 	string name, productName;
-	int age,quantity;
+	int age,quantity, productNumber;
 	double price, weight, height;
 
 	void PatientData() {
@@ -14,16 +15,9 @@ struct Hospital {
 			<< "Patient name: " << name << "\n"
 			<< "Patient age: " << age << "\n"
 			<< "Patient height: " << height << "\n"
-			<< "Patient weight: " << weight << "\n";
+			<< "Patient weight: " << weight << "\n"
+			<< "-----------------------\n";
 	}
-
-	void ProductData() {
-		cout << "\n-----------------------\n"
-			<< "Product name: " << productName << "\n"
-			<< "Product quantity: " << quantity << "\n"
-			<< "Product price: " << price << "\n";
-	}
-
 };
 
 struct NodePatients {
@@ -44,8 +38,10 @@ void AddPatientData(Hospital&);
 void AddRecord(Hospital&, NodePatients*&, NodePatients*&);
 void ViewRecord(NodePatients*&);
 void AddProductData(Hospital&);
+void ClearPatientRecord(NodePatients*&);
 void AddProductRecord(Hospital&, NodeProducts*&, NodeProducts*&);
 void ViewProducts(NodeProducts*&);
+void BuyProducts(Hospital&, NodeProducts*&, NodeProducts*&);
 void ComputeBMI(Hospital&, NodePatients*&);
 
 
@@ -54,70 +50,102 @@ int main()
 	Hospital patient, product;
 	NodePatients* headPatient = NULL, * tailPatient = NULL;
 	NodeProducts* headProduct = NULL, * tailProduct = NULL;
-	int employeeChoice, patientChoice, role = 0; 
+	int employeeChoice, patientChoice, pickRole = 0; 
+	string employeeName, employeePassword;
 	
 	do {
-		role = ChooseRole();
+			pickRole = ChooseRole();
 
-		if (role == 1) {
+			if (pickRole == 1) {
+			
+				do {
+					do {
+						patientChoice = PatientMainMenu();
+						switch (patientChoice)
+						{
+						case 1:
+							break;
+						case 2:
+							ViewProducts(headProduct);
+							BuyProducts(product, headProduct, tailProduct);
+							break;
+						case 3:
+
+							break;
+						case 4:
+							cout << ".";
+							system("CLS");
+							break;
+						case 0:
+							cout << "Thank you for using the program! \n";
+							system("pause");
+							exit(0);
+						default:
+							cout << "Incorrect choice\n";
+							system("pause");
+							system("CLS");
+							break;
+						}
+					} while (patientChoice != 4);
+
+				} while (patientChoice >= 1 && patientChoice <= 3);
+			}
+		
+
+		else if (pickRole == 2) {
+		
+				do {
+					system("CLS");
+					cout << "Enter employee name: "; cin >> employeeName;
+					cout << "Enter employee password: "; cin >> employeePassword;
+
+				} while (employeeName != "Adrian" || employeePassword != "IloveyousomuchZyrah");
+			
+			system("CLS");
 			do {
-				patientChoice = PatientMainMenu();
-				switch (patientChoice)
-				{
-				case 1:
-					ViewProducts(headProduct);
-					break;
-				case 2:
+				do {
+					employeeChoice = EmployeeMainMenu();
+					switch (employeeChoice)
+					{
+					case 1:
+						AddPatientData(patient);
+						AddRecord(patient, headPatient, tailPatient);
+						break;
+					case 2:
+						ViewRecord(headPatient);
+						break;
+					case 3:
+						AddProductData(product);
+						AddProductRecord(product, headProduct, tailProduct);
+						break;
+					case 4:
+						ViewProducts(headProduct);
+						break;
+					case 5:
+						ComputeBMI(patient, headPatient);
+						break;
+					case 6:
+						ClearPatientRecord(headPatient);
+					case 7:
+						cout << "Returning to main menu\n";
+						system("pause");
+						system("CLS");
+						break;
+					case 0:
+						cout << "Thank you for using the program! \n";
+						system("pause");
+						exit(0);
+					default:
+						cout << "Incorrect choice\n";
+						system("pause");
+						system("CLS");
+						break;
+					}
 					
-					break;
-				case 3:
-
-					break;
-				case 0:
-					cout << "Thank you for using the program! \n";
-					system("pause");
-					exit(0);
-				default:
-					break;
-				}
-			} while (patientChoice >= 1 || patientChoice <= 3);
-
+				} while (employeeChoice != 7);
+				
+			} while (employeeChoice >= 1 && employeeChoice <= 6);
 		}
-
-
-		else if (role == 2) {
-
-			do {
-				employeeChoice = EmployeeMainMenu();
-				switch (employeeChoice)
-				{
-				case 1:
-					AddPatientData(patient);
-					AddRecord(patient, headPatient, tailPatient);
-					break;
-				case 2:
-					ViewRecord(headPatient);
-					break;
-				case 3:
-					AddProductData(product);
-					AddProductRecord(product, headProduct, tailProduct);
-					break;
-				case 4:
-					ViewProducts(headProduct);
-					break;
-				case 5:
-					ComputeBMI(patient, headPatient);
-					break;
-				case 0:
-					cout << "Thank you for using the program! \n";
-					system("pause");
-					exit(0);
-				default:
-					break;
-				}
-			} while (employeeChoice >= 1 || employeeChoice <= 4);
-		}
-
 
 	} while (true);
 	return 0;
@@ -146,7 +174,6 @@ int ChooseRole() {
 	} while (role < 1 || role > 2);
 
 	return role;
-
 }
 
 
@@ -155,13 +182,15 @@ int EmployeeMainMenu() {
 	int menuChoice;
 
 	cout << "==============================================\n"
-		<< "                 Hospital Menu\n"
+		<< "                 Employee Menu\n"
 		<< "==============================================\n"
 		<< "1. Add patient record\n"
 		<< "2. View patient records\n"
 		<< "3. Add product list\n"
 		<< "4. View product list\n"
 		<< "5. Compute BMI\n"
+		<< "6. Clear patient record\n"
+		<< "7. Go back to choose role\n"
 		<< "0. Exit\n"
 		<< "\nEnter choice here: ";
 	cin >> menuChoice;
@@ -175,11 +204,12 @@ int PatientMainMenu() {
 	int patientChoice;
 
 	cout << "==============================================\n"
-		<< "                 Hospital Menu\n"
+		<< "                 Patient Menu\n"
 		<< "==============================================\n"
 		<< "1. View product list\n"
 		<< "2. Buy products\n"
 		<< "3. History\n"
+		<< "4. Go back to choose role\n"
 		<< "0. Exit\n"
 		<< "\nEnter choice here: ";
 	cin >> patientChoice;
@@ -187,8 +217,6 @@ int PatientMainMenu() {
 	system("CLS");
 
 	return patientChoice;
-
-
 }
 
 void AddPatientData(Hospital& patientInfo) {
@@ -232,12 +260,37 @@ void ViewRecord(NodePatients*& headPatient) {
 		}
 
 	}
+	cout << "\n";
 	system("pause");
 	system("CLS");
+}
 
+void ClearPatientRecord(NodePatients*& headPatient)
+{
+	NodePatients* temp = NULL, * current = headPatient;
+
+	if (!headPatient)
+		cout << "No Patient Records Yet!\n\n";
+	else
+	{
+		while (temp)
+		{
+			temp = current->next;
+			delete current;
+			current = temp;
+		}
+
+		cout << "Patient Record Cleared!\n";
+	}
+
+	cout << "\n";
+	system("pause");
+	system("cls");
 }
 
 void AddProductData(Hospital& productInfo) {
+	cout << "Enter product number: ";
+	cin >> productInfo.productNumber;
 	cout << "Enter product name: ";
 	cin >> productInfo.productName;
 	cout << "Enter product quantity: ";
@@ -271,13 +324,56 @@ void ViewProducts(NodeProducts*& headProduct) {
 	else {
 		while (temp)
 		{
-			temp->products.ProductData();
+			cout << "\n-----------------------\n"
+				<< "Product number: " << temp->products.productNumber << "\n"
+				<< "Product name: " << temp->products.productName << "\n"
+				<< "Product quantity: " << temp->products.quantity << "\n"
+				<< "Product price: " << temp->products.price << "\n"
+				<< "-----------------------\n";
 			temp = temp->next;
 		}
 	}
+	cout << "\n";
 	system("pause");
 	system("CLS");
+}
 
+void BuyProducts(Hospital& productInfo, NodeProducts*& headProduct, NodeProducts*& tailProduct) {
+	NodeProducts* current = headProduct;
+	int chooseProducts, chooseProductQuantity;
+	double totalAmount = 0;
+
+	cout << "Enter product number: ";
+	cin >> chooseProducts;
+
+	while (current != nullptr && current->products.productNumber != chooseProducts) {
+		current = current->next;
+	}
+
+	if (current == nullptr) {
+		cout << "Product not found!" << endl;
+		return;
+	}
+
+	cout << "\n\nChosen product details:\n"
+		<< "Product Name: " << current->products.productName << "\n"
+		<< "Product Quantity: " << current->products.quantity << "\n"
+		<< "Product Price: " << current->products.price << "\n\n";
+
+	cout << "Choose how many quantity: ";
+	cin >> chooseProductQuantity;
+
+	if (chooseProductQuantity > current->products.quantity) {
+		cout << "Not enough quantity available!" << endl;
+	}
+	else {
+		current->products.quantity -= chooseProductQuantity;
+		totalAmount = chooseProductQuantity * current->products.price;
+		cout << "Total amount for chosen products: " << totalAmount << endl;
+	}
+
+	system("pause");
+	system("CLS");
 }
 
 void ComputeBMI(Hospital& patientInfo, NodePatients*& headPatient) {
@@ -290,14 +386,14 @@ void ComputeBMI(Hospital& patientInfo, NodePatients*& headPatient) {
 	else {
 		while (temp != nullptr) {
 			bodyMassIndex = ((temp->patient.weight) * 703 / (temp->patient.height * temp->patient.height));
-			cout << "Patient Name: " << temp->patient.name << endl;
-			cout << "BMI: " << bodyMassIndex << endl;
-			cout << "----------------------------------" << endl;
+			cout << "----------------------------------\n"
+			     << "Patient Name: " << temp->patient.name;
+			cout << "\nBMI: " << bodyMassIndex;
+			cout << "\n----------------------------------\n";
 			temp = temp->next;
 		}
 
 		system("pause");
 		system("CLS");
 	}
-
 }
